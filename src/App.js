c20f6129b64c38f5fca9d9d6987673c57efed219
themostretarded <marcosresidentevil@hotmail.com>
2017-07-12 17:34:44 -0500

    primer commit que hago a ver si jala

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import osos from './oso.png'
 import './App.css';
 
@@ -21,20 +20,31 @@ class App extends Component {
                 </ul>
         </div>
         <p className="App-intro">La fabrica de software es un apartado de la fime para el desarrollo de diferentes proyectos para la universidad pero principal mente para la fime</p>
-        
         <span id="quienes"></span>
+            <section>
             <div className="App-Cuerpo">Somos un grupo de estudiantes de fime comprometidos con el desarrollo de nuestras habilidades como ingenieros en software</div>
+            </section>
         <span id="hacemos"></span>
+            <section>
             <div className="App-Cuerpo">Tratamos de inovar nuestra facultad con proyectos utiles</div>
+            </section>
         <span id="equipo"></span>
+            <section>
             <div className="App-Cuerpo">Grupo de estudiantes"Posibles tarjetas con redes sociales y puesto"</div>
+            </section>
         <span id="idea"></span>
+            <section>
             <div className="App-Cuerpo">Usted persona o empresa tiene alguna idea para su negocio, No dude en contactarnos nosotros nos encargamos</div>
+            </section>
         <span id="contacto"></span>
+            <section>
             <div className="App-Cuerpo">Tel:666-666-66-6</div>
+            </section>
         <span id="redes"></span>
+            <section>
             <div className="App-Cuerpo">facebook? pagina web? algo?</div>
-        </div>        
+            </section>
+        </div>
     );
   }
 }
