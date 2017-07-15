7a178801304725c5ab098eb23c6009c994ebfa2d
themostretarded <marcosresidentevil@hotmail.com>
2017-07-14 20:27:16 -0500

    bower instalado y ya jalan los css de mui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import osos from './oso.png'
 import './App.css';
 import Appbar from 'muicss/lib/react/appbar';
+import Button from 'muicss/lib/react/button';
 
 class App extends Component {
   render() {
@@ -9,7 +10,7 @@ class App extends Component {
       <div className="App">
         <Appbar id="inicio">
             <img src={osos} className="App-logo" alt="logo" />
-                <ul>
+            <ul>
                 <li><a href="#inicio">Inicio|</a></li>
                 <li><a href="#quienes">Quiénes Somos|</a></li>
                 <li><a href="#hacemos">Qué hacemos|</a></li>
@@ -17,10 +18,9 @@ class App extends Component {
                 <li><a href="#idea">Tienes una idea|</a></li>
                 <li><a href="#contacto">Contactanos|</a></li>
                 <li><a href="#redes">Redes</a></li>
-                </ul>
+            </ul>
         </Appbar>
-        
-        </div>
+    </div>
     );
   }
 }
