5253810d386dd34029fea23724c7f7acc96bf9ea
themostretarded <marcosresidentevil@hotmail.com>
2017-07-24 19:26:17 -0500

    Borre liga al fondo

--- src/Informacion.js
+++ src/Informacion.js
@@ -7,7 +7,6 @@ class Informacion extends Component {
     return (
         <Appbar>
         <h3>Mapa del sitio</h3>
-        <a href="http://www.freepik.com/free-photos-vectors/background">Background image created by Kjpargeter - Freepik.com</a>
         </Appbar>
     );
   }
