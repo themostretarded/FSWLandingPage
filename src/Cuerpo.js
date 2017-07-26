2c74b50cbef0244a7147efbf89412136e0a3fce5
themostretarded <marcosresidentevil@hotmail.com>
2017-07-25 21:16:42 -0500

    cambios en el tamaño del texto

--- src/Cuerpo.js
+++ src/Cuerpo.js
@@ -9,33 +9,33 @@ class Cuerpo extends Component {
         <div className="App-Cuerpo">
             <br></br>
             <Panel className="mui--z4">
-                <h1>Bienvenido a la Fabrica de Software</h1>
-                <h2>La fabrica de software es un apartado de la fime para el desarrollo de diferentes proyectos para la universidad pero principal mente para la fime</h2>
+                <h2>Bienvenido a la Fabrica de Software</h2>
+                <h3>La fabrica de software es un apartado de la fime para el desarrollo de diferentes proyectos para la universidad pero principal mente para la fime</h3>
             </Panel>
         
             <Panel className="mui--z4">
                 <span id="hacemos" ></span>
-                <h1>Que hacemos</h1>
-                <h2>Tratamos de inovar nuestra facultad con proyectos utiles</h2>
+                <h2>Que hacemos</h2>
+                <h3>Tratamos de inovar nuestra facultad con proyectos utiles</h3>
             </Panel>
             
             <Panel className="mui--z4">
                 <span id="equipo"></span>
-                        <h1>Nuestro Equipo</h1>
-                        <h2>Grupo de estudiantes"Posibles tarjetas con redes sociales y puesto"</h2>
+                        <h2>Nuestro Equipo</h2>
+                        <h3>Grupo de estudiantes"Posibles tarjetas con redes sociales y puesto"</h3>
             </Panel>
         
             <Panel className="mui--z4">
                 <span id="idea"></span>
-                <h1>Tienes una idea</h1>
-                <h2>Usted persona o empresa tiene alguna idea para su negocio, No dude en contactarnos nosotros nos encargamos</h2>
+                <h2>Tienes una idea</h2>
+                <h3>Usted persona o empresa tiene alguna idea para su negocio, No dude en contactarnos nosotros nos encargamos</h3>
             </Panel>
         
             <Panel className="mui--z4">
                 <span id="redes"></span>
-                <h1>Nuestros medios</h1>
+                <h2>Nuestros medios</h2>
                 <h3>facebook? pagina web? algo?</h3>
-                <div className="mui--text-body2">Si te interesa formar parte del equipo no dudes en contactarnos</div>
+                <h3>Si te interesa formar parte del equipo no dudes en contactarnos</h3>
             </Panel>
         </div>
     );
