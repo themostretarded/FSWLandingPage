662e775c21643c83bb4f6b15c60a69d3ecd818d8
ErickFlorecz <38997152+ErickFlorecz@users.noreply.github.com>
2019-01-24 10:05:02 -0600

    ReHaciendo toda la pagina en una sola
    Ahora se puso todo en una sola pagina

--- js/imagnes.js
+++ js/imagnes.js
@@ -1,7 +1,7 @@
 
 window.onload = function(){
 
-    var imagen = document.getElementById('imgDinamica');
+    var imagen = document.getElementById('imgCajeros');
     imagen.addEventListener('mouseover', function (){
     var rnd = Math.floor((Math.random() * 3) + 1);
                         
@@ -10,10 +10,27 @@ window.onload = function(){
         this.src='./img/Portafolio/Cajeros/Cajero_01.gif';
     break;
     case 2:
-        this.src='./img/Portafolio//Cajeros/Cajero_02.gif';
+        this.src='./img/Portafolio/Cajeros/Cajero_02.gif';
     break;
     case 3:
         this.src='./img/Portafolio/Cajeros/Cajero_03.gif';
+    break;
+    }   
+    });
+
+    var imagen = document.getElementById('imgLockers');
+    imagen.addEventListener('mouseover', function (){
+    var rnd = Math.floor((Math.random() * 3) + 1);
+                        
+    switch(rnd){
+    case 1:
+        this.src='./img/Portafolio/Lockers/pasillo_02.jpg';
+    break;
+    case 2:
+        this.src='./img/Portafolio//Lockers/pasillo_03.jpg';
+    break;
+    case 3:
+        this.src='./img/Portafolio/Lockers/registro_01.jpg';
     break;
     }   
     });
