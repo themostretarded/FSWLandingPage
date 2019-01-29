faa11f951bdf2718d078b9e8c387b4bcda8f1221
ErickFlorecz <ErickFlorecz@users.noreply.github.com>
2019-01-29 17:12:21 -0600

    Cambios basicos
    AL chile ni me acuerdo lo hice hace una semana

--- js/imagnes.js
+++ js/imagnes.js
@@ -1,13 +1,13 @@
 
 window.onload = function(){
-
+    //Cajeros
     var imagen = document.getElementById('imgCajeros');
     imagen.addEventListener('mouseover', function (){
     var rnd = Math.floor((Math.random() * 3) + 1);
                         
     switch(rnd){
     case 1:
-        this.src='./img/Portafolio/Cajeros/Cajero_01.gif';
+        this.src='./img/Portafolio/Cajeros/Cajero_01.mp4';
     break;
     case 2:
         this.src='./img/Portafolio/Cajeros/Cajero_02.gif';
@@ -17,7 +17,24 @@ window.onload = function(){
     break;
     }   
     });
-
+    //Lockers
+    var imagen = document.getElementById('imgLockers');
+    imagen.addEventListener('mouseover', function (){
+    var rnd = Math.floor((Math.random() * 3) + 1);
+                        
+    switch(rnd){
+    case 1:
+        this.src='./img/Portafolio/Lockers/pasillo_02.jpg';
+    break;
+    case 2:
+        this.src='./img/Portafolio//Lockers/pasillo_03.jpg';
+    break;
+    case 3:
+        this.src='./img/Portafolio/Lockers/registro_01.jpg';
+    break;
+    }   
+    });
+    //Servicios Generales
     var imagen = document.getElementById('imgLockers');
     imagen.addEventListener('mouseover', function (){
     var rnd = Math.floor((Math.random() * 3) + 1);
